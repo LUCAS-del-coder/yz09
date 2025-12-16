b1e56dd3a07747bde0e621c26011d969c36a524c
IDEA3C <idea3c@IDEA3CdeMacBook-Air.local>
2025-12-16 16:55:51 +0800

    Fix: Add file extensions to webpack resolve config

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,7 @@ const nextConfig = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname),
     };
+    config.resolve.extensions.push('.ts', '.tsx', '.js', '.jsx', '.json');
     return config;
   },
 };
