0c0f3ff196e80c161d807cf0a9f6f2bf70f30ba8
high <highsky33570@gmail.com>
2026-02-03 11:16:39 +0800

    Merge branch 'main' of https://github.com/LUCAS-del-coder/yz09 into feature/seo_dev

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,13 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.mm7.tech',
+        pathname: '/wp-content/uploads/**',
+      },
+    ],
   },
   
   experimental: {
